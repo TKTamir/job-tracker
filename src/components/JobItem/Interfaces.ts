6d4e8f09274b3457f0cc16e7b3b3cddaf2178750
tamir <tamirkhy@gmail.com>
2025-02-04 17:39:14 +0200

    Create JobItemProps interface

---
+++ src/components/JobItem/Interfaces.ts
@@ -0,0 +1,12 @@
+export interface JobItemProps {
+  jobList: {
+    applicationDate: string;
+    companyName: string;
+    companyWebsite: string;
+    jobAd: string;
+    positionName: string;
+    progression: string;
+    requestedSalary: string;
+    status: string;
+  }
+}
