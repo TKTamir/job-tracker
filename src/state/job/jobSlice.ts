5940b1691122584a4380730138bf3c5c4e40978c
tamir <tamirkhy@gmail.com>
2025-02-05 18:25:01 +0200

    Add todo to jobSlice

--- src/state/job/jobSlice.ts
+++ src/state/job/jobSlice.ts
@@ -3,16 +3,28 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface JobState {
   jobs: JobItemProps[];
+  currentJob: JobItemProps;
 }
 
 const initialState: JobState = {
   jobs: [],
+  currentJob: {
+    applicationDate: "",
+    companyName: "",
+    companyWebsite: "",
+    jobAd: "",
+    positionName: "",
+    progression: "",
+    requestedSalary: "",
+    status: "",
+  },
 };
 
 const jobSlice = createSlice({
   name: "jobs",
   initialState,
   reducers: {
+    // TODO: convert to an sync thunk that calls an API when backend is built
     addJob: (state, action: PayloadAction<JobItemProps>) => {
       state.jobs.push(action.payload);
     },
