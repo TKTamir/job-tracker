dcfdd21a10587f606ab1eec89a66e2fdad3547da
tamir <tamirkhy@gmail.com>
2025-02-05 18:42:00 +0200

    Adjust AddJob to dispatch local state to redux

--- src/components/AddJob/AddJob.tsx
+++ src/components/AddJob/AddJob.tsx
@@ -1,16 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
+import {JobItemProps} from "../JobItem/Interfaces.ts";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "../../state/store.ts";
+import {addJob} from "../../state/job/jobSlice.ts";
+
 
 const AddJob: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [jobData, setJobData] = useState<JobItemProps>({
+    applicationDate: "",
+    companyName: "",
+    companyWebsite: "",
+    jobAd: "",
+    positionName: "",
+    progression: "",
+    requestedSalary: "",
+    status: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setJobData({
+      ...jobData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!jobData.companyName || !jobData.positionName) {
+      alert("Company Name and Position are required.");
+      return;
+    }
+    console.log(jobData);
+    dispatch(addJob(jobData));
+
+    setJobData({
+      applicationDate: "",
+      companyName: "",
+      companyWebsite: "",
+      jobAd: "",
+      positionName: "",
+      progression: "",
+      requestedSalary: "",
+      status: "",
+    });
+  };
 
   return (
     <div className="AddJob">
       <h2 className="text-lg font-semibold mt-4 mb-4">Add New Job</h2>
-      <form className="flex flex-col gap-3">
-        <input type="text" placeholder="Company Name"/>
-        <input type="text" placeholder="Position"/>
-        <input type="text" placeholder="Requested Salary"/>
-        <input type="text" placeholder="Application Date"/>
-        <button type="submit" onClick={() => alert('Job Saved')}>Save Job</button>
+      <form onSubmit={handleSubmit} className="AddJob flex flex-col gap-3">
+        <input
+          type="text"
+          name="companyName"
+          placeholder="Company Name"
+          value={jobData.companyName}
+          onChange={handleInputChange}
+          className="border p-2 rounded"
+          required
+        />
+        <input
+          type="text"
+          name="positionName"
+          placeholder="Position"
+          value={jobData.positionName}
+          onChange={handleInputChange}
+          className="border p-2 rounded"
+          required
+        />
+        <input
+          type="text"
+          name="applicationDate"
+          placeholder="Application Date"
+          value={jobData.applicationDate}
+          onChange={handleInputChange}
+          className="border p-2 rounded"
+          required
+        />
+        <button type="submit">Save Job</button>
       </form>
     </div>
   )
