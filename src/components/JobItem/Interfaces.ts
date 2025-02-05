c13bf36e3a2414825c381ad424185474f68427fd
tamir <tamirkhy@gmail.com>
2025-02-05 17:55:48 +0200

    Adjust JobItemProps interface

--- src/components/JobItem/Interfaces.ts
+++ src/components/JobItem/Interfaces.ts
@@ -1,12 +1,10 @@
 export interface JobItemProps {
-  jobList: {
-    applicationDate: string;
-    companyName: string;
-    companyWebsite: string;
-    jobAd: string;
-    positionName: string;
-    progression: string;
-    requestedSalary: string;
-    status: string;
-  }
+  applicationDate: string;
+  companyName: string;
+  companyWebsite: string;
+  jobAd: string;
+  positionName: string;
+  progression: string;
+  requestedSalary: string;
+  status: string;
 }
