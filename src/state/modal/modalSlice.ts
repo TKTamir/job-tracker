8934f9c5f32cbfff1fc20f5bb259e81571da879c
tamir <tamirkhy@gmail.com>
2025-02-06 16:29:48 +0200

    Add modalType to modalSlice

--- src/state/modal/modalSlice.ts
+++ src/state/modal/modalSlice.ts
@@ -1,22 +1,26 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface ModalState {
   isModalOpen: boolean;
+  modalType: "REGISTER" | "LOGIN" | "ADD_JOB" | null;
 }
 
 const initialState: ModalState = {
   isModalOpen: false,
+  modalType: null,
 }
 
 const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    openModal(state) {
+    openModal: (state, action: PayloadAction<ModalState["modalType"]>) => {
       state.isModalOpen = true;
+      state.modalType = action.payload;
     },
     closeModal(state) {
       state.isModalOpen = false;
+      state.modalType = null;
     },
   },
 });
