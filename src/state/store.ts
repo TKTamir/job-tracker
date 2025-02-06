7d759a49be16c466b73980ee4171c9c1ca28f5be
tamir <tamirkhy@gmail.com>
2025-02-06 14:09:25 +0200

    Add reducer to store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import jobReducer from "./job/jobSlice.ts";
+import modalReducer from "./modal/modalSlice.ts";
 
 export const store = configureStore({
   reducer: {
     jobs: jobReducer,
+    modal: modalReducer,
   }
 });
 
