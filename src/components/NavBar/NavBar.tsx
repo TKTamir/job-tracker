c1a5204b9f140c7363d948a4e25efd6328fa9e8e
tamir <tamirkhy@gmail.com>
2025-02-11 19:01:53 +0200

    Handle logout in NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Modal from "../Modal/Modal.tsx";
-import {useDispatch} from "react-redux";
-import {AppDispatch} from "../../state/store.ts";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootState} from "../../state/store.ts";
 import {openModal} from "../../state/modal/modalSlice.ts";
 import {setSearchQuery} from "../../state/search/searchSlice.ts";
 import {ModalTypes} from "./Interfaces.ts";
+import {logoutUser} from "../../state/auth/authSlice.ts";
 
 const NavBar: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
   const handleOpenModal = (modalType: ModalTypes) => {
     dispatch(openModal(modalType));
@@ -15,6 +17,10 @@ const NavBar: React.FC = () => {
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setSearchQuery(e.target.value));
+  };
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
   };
 
   return (
@@ -25,9 +31,17 @@ const NavBar: React.FC = () => {
         onChange={handleSearchChange}
       />
       <button className="m-2" onClick={() => handleOpenModal("ADD_JOB")}>+</button>
-      <div className="">
-        <button className="m-2" onClick={() => handleOpenModal("REGISTER")}>Register</button>
-        <button className="m-2" onClick={() => handleOpenModal("LOGIN")}>Log in</button>
+      <div className="UserButtons">
+        {isAuthenticated ? (
+          <>
+            <button className="m-2" onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <button className="m-2" onClick={() => handleOpenModal("REGISTER")}>Register</button>
+            <button className="m-2" onClick={() => handleOpenModal("LOGIN")}>Log in</button>
+          </>
+        )}
       </div>
       <Modal/>
     </div>
