d1009ae595039c38daf829969deff1eea775f987
tamir <tamirkhy@gmail.com>
2025-02-17 17:16:33 +0200

    Add modalProps to ModalState

--- src/state/modal/modalSlice.ts
+++ src/state/modal/modalSlice.ts
@@ -1,27 +1,38 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {ModalTypes} from "../../components/NavBar/Interfaces.ts";
+import {IJobItem} from "../../components/JobItem/Interfaces.ts";
 
 export interface ModalState {
   isModalOpen: boolean;
-  modalType: ModalTypes;
+  modalType: ModalTypes
+  modalProps?: IJobItem["id"];
 }
+
+type OpenModalPayload = {
+  modalType: ModalState['modalType'];
+  modalProps?: ModalState['modalProps'];
+};
+
 
 const initialState: ModalState = {
   isModalOpen: false,
   modalType: null,
+  modalProps: undefined,
 }
 
 const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    openModal: (state, action: PayloadAction<ModalState["modalType"]>) => {
+    openModal: (state, action: PayloadAction<OpenModalPayload>) => {
       state.isModalOpen = true;
-      state.modalType = action.payload;
+      state.modalType = action.payload.modalType;
+      state.modalProps = action.payload.modalProps;
     },
     closeModal(state) {
       state.isModalOpen = false;
       state.modalType = null;
+      state.modalProps = undefined;
     },
   },
 });
