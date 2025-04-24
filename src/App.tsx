5f77ac1f25891ae0a16095c0c12e030814576898
tamir <tamirkhy@gmail.com>
2025-04-24 15:23:05 +0300

    load user from token on app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './App.css'
 import Home from "./pages/Home/Home.tsx";
+import {useDispatch} from "react-redux";
+import {loadUserFromToken} from "./state/auth/authSlice.ts";
+import {AppDispatch} from "./state/store.ts";
 
 const App: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(loadUserFromToken());
+  }, [dispatch]);
 
   return (
     <div>
