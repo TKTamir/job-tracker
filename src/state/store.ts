cd75183ef65bf31de733b29e45312f1e319dd471
tamir <tamirkhy@gmail.com>
2025-05-05 18:41:51 +0300

    Add middleware to store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,22 @@
 import {configureStore} from "@reduxjs/toolkit";
-import jobReducer from "./job/jobSlice.ts";
+import {jobsApi} from "./api/jobsApi.ts";
+import {authApi} from "./api/authApi.ts";
+import authReducer from "./auth/authSlice.ts";
 import modalReducer from "./modal/modalSlice.ts";
 import searchReducer from "./search/searchSlice.ts";
-import authReducer from "./auth/authSlice.ts";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    jobs: jobReducer,
     modal: modalReducer,
     search: searchReducer,
-  }
+    [authApi.reducerPath]: authApi.reducer,
+    [jobsApi.reducerPath]: jobsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(jobsApi.middleware)
+      .concat(authApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
