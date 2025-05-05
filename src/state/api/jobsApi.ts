17108de861353b611cc7aaf819f39c72cdbe2626
tamir <tamirkhy@gmail.com>
2025-05-05 16:10:46 +0300

    Create jobsapi

---
+++ src/state/api/jobsApi.ts
@@ -0,0 +1,52 @@
+import {createApi} from '@reduxjs/toolkit/query/react';
+import {IJobItem} from "../../components/JobItem/Interfaces.ts";
+import {baseQuery} from "./baseQuery.ts";
+
+
+export const jobsApi = createApi({
+  reducerPath: 'jobsApi',
+  baseQuery: baseQuery,
+  tagTypes: ['Job'],
+  endpoints: (builder) => ({
+    getJobs: builder.query<IJobItem[], number | undefined>({
+      query: (userId) => {
+        const url = 'jobs';
+        if (userId) {
+          return `${url}?userId=${userId}`;
+        }
+        return url;
+      },
+      providesTags: ['Job'],
+    }),
+    addJob: builder.mutation<IJobItem, Partial<IJobItem>>({
+      query: (job) => ({
+        url: 'jobs',
+        method: 'POST',
+        body: job,
+      }),
+      invalidatesTags: ['Job'],
+    }),
+    updateJob: builder.mutation<IJobItem, Partial<IJobItem>>({
+      query: (job) => ({
+        url: `jobs/${job.id}`,
+        method: 'PATCH',
+        body: job,
+      }),
+      invalidatesTags: ['Job'],
+    }),
+    deleteJob: builder.mutation<{ success: boolean; id: number }, number>({
+      query: (id) => ({
+        url: `jobs/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Job'],
+    }),
+  }),
+});
+
+export const {
+  useGetJobsQuery,
+  useAddJobMutation,
+  useUpdateJobMutation,
+  useDeleteJobMutation,
+} = jobsApi;
