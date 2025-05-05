caf425d48a0a161aaafed64dc1a4771f7e00c142
tamir <tamirkhy@gmail.com>
2025-05-05 18:22:39 +0300

    Move auth interfaces to a types file

---
+++ src/state/types/auth.ts
@@ -0,0 +1,37 @@
+export interface AuthResponse {
+  message: string;
+  token: string;
+}
+
+export interface AuthState {
+  user: User | null;
+  token: string | null;
+}
+
+export interface DecodedToken {
+  id: number;
+  email: string;
+  name: string;
+  exp: number;
+  iat: number;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterCredentials {
+  email: string;
+  password: string;
+  name: string;
+}
+
+export interface User {
+  id: number;
+  email: string;
+  name: string;
+  token: string;
+}
+
+
