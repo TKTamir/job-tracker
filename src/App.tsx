4566c30d8f581a682d3915545f2351e774da706a
tamir <tamirkhy@gmail.com>
2025-05-06 14:30:38 +0300

    Replace loadUserFromToken with setUserFromToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, {useEffect} from "react";
+import {useDispatch} from "react-redux";
 import './App.css'
 import Home from "./pages/Home/Home.tsx";
-import {useDispatch} from "react-redux";
-import {loadUserFromToken} from "./state/auth/authSlice.ts";
+import {setUserFromToken} from "./state/auth/authSlice.ts";
 import {AppDispatch} from "./state/store.ts";
+
 
 const App: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    dispatch(loadUserFromToken());
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(setUserFromToken(token));
+    }
   }, [dispatch]);
+
 
   return (
     <div>
