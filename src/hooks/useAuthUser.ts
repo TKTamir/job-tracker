6bba4040aaa21407ace64380b28ac83a94057eb6
tamir <tamirkhy@gmail.com>
2025-05-06 15:25:19 +0300

    Remove useAuthUser.ts

--- src/hooks/useAuthUser.ts
+++
@@ -1,6 +0,0 @@
-import {useSelector} from "react-redux";
-import {RootState} from "../state/store";
-
-export const useAuthUser = () => {
-  return useSelector((state: RootState) => state.auth.user);
-};
