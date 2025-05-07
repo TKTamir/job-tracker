b2ba934949cf5b7e2fe5385e7868371161b4343f
tamir <tamirkhy@gmail.com>
2025-05-07 18:38:49 +0300

    Add heading and paragraph to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -29,6 +29,11 @@ const Home: React.FC = () => {
   return (
     <div className="Home flex flex-col justify-center items-center">
       <div>
+        <h1 className="m-4 font-bold text-4xl">Take Control of Your Job Hunt!</h1>
+        <p className="m-2 text-md">Streamline your search, discover opportunities, and land your dream job with
+          ease.</p>
+      </div>
+      <div>
         <h2 className="m-4 font-bold text-3xl">Features</h2>
       </div>
       <div className="flex flex-row m-2">
