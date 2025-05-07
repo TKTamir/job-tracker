55d5bc06b58e3ad42d9a9e47bbca3f57edb88c7f
tamir <tamirkhy@gmail.com>
2025-05-07 18:02:33 +0300

    Create Card.tsx

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {CardProps} from "./interfaces.ts";
+
+const Card: React.FC<CardProps> = ({icon, description, subtitle, title}) => {
+
+  return (
+    <div className="Card flex flex-col h- 50 max-w-52">
+      <img alt="card-icon" src={icon}></img>
+      <div className="flex flex-col">
+        <h3 className="m-2 font-bold text-lg">{title}</h3>
+        <h5 className="m-2 text-xs">{subtitle}</h5>
+        <p className="m-2 text-md">{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
