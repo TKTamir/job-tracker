65223439dc19665a56ca804453ad4469b8bcc6b7
tamir <tamirkhy@gmail.com>
2025-05-08 14:22:34 +0300

    Map job fields in JobApplications

--- src/components/JobApplications/JobApplications.tsx
+++ src/components/JobApplications/JobApplications.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import {useSelector} from "react-redux";
 import {useGetJobsQuery} from "../../state/api/jobsApi.ts";
 import {RootState} from "../../state/store.ts";
+import {IJobItem} from "../JobItem/Interfaces.ts";
 
 const JobApplications: React.FC = () => {
   const user = useSelector((state: RootState) => state.auth.user);
   const {data: jobs, isLoading, isError, error} = useGetJobsQuery(user?.id, {skip: !user?.id});
+
   if (!user) return;
+
   const fields = [
     {name: "companyName", label: "Company Name"},
     {name: "positionName", label: "Position"},
@@ -24,8 +27,23 @@ const JobApplications: React.FC = () => {
   }
 
   return (
-    <div className="JobApplications">
-
+    <div className="JobApplications p-4">
+      <div
+        className="grid grid-cols-4 gap-4 text-left items-start"
+      >
+        {fields.map((field) => (
+          <div key={`header-${field.name}`} className="font-semibold">
+            {field.label}
+          </div>
+        ))}
+        {jobs?.map((job: IJobItem) =>
+          fields.map((field) => (
+            <div key={`${job.id}-${field.name}`}>
+              {job[field.name as keyof IJobItem] || "N/A"}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   )
 }
