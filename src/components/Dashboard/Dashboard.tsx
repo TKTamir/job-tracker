93ef91cd6f6c3e35279522e10903c71f7795d74e
tamir <tamirkhy@gmail.com>
2025-05-08 14:32:03 +0300

    Allow adding applications from the dashboard, adjust styling

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,29 @@
 import React from "react";
+import {useDispatch} from "react-redux";
 import JobsList from "../JobsList/JobsList.tsx";
+import JobApplications from "../JobApplications/JobApplications.tsx";
+import {openModal} from "../../state/modal/modalSlice.ts";
+import {AppDispatch} from "../../state/store.ts";
+import {ModalTypes} from "../NavBar/Interfaces.ts";
 
 const Dashboard: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
 
+  const handleOpenModal = (modalType: ModalTypes) => {
+    dispatch(openModal({modalType}));
+  };
   return (
-    <div className="Dashboard">
+    <div className="Dashboard flex flex-col items-center">
       <div className="justify-self-center">
         <h1 className="m-2 block">Dashboard</h1>
       </div>
-      <div className="justify-self-center">
+      <div>
+        <button className="m-2" onClick={() => handleOpenModal("ADD_JOB")}>Add New Application
+        </button>
+      </div>
+      <div className="m-2">
         <JobsList/>
+        <JobApplications/>
       </div>
     </div>
   )
