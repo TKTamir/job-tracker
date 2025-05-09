453b8fc0ff42a277b476e789cf6acae9154811ff
tamir <tamirkhy@gmail.com>
2025-05-09 12:38:55 +0300

    create CustomFetchError type

--- src/components/Login/Interfaces.ts
+++ src/components/Login/Interfaces.ts
@@ -1,4 +1,10 @@
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+
 export interface LoginData {
   email: string;
   password: string;
 }
+
+export type CustomFetchError = FetchBaseQueryError & {
+  data: { message: string };
+};
