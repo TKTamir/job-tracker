dc8e20071896fe8ce6c1742f7ed105f43f810640
tamir <tamirkhy@gmail.com>
2025-05-11 18:47:18 +0300

    Remove CustomFetchError type

--- src/components/Login/Interfaces.ts
+++ src/components/Login/Interfaces.ts
@@ -1,10 +1,4 @@
-import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
-
 export interface LoginData {
   email: string;
   password: string;
 }
-
-export type CustomFetchError = FetchBaseQueryError & {
-  data: { message: string };
-};
