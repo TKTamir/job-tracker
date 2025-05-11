b81bf6d65636ee907761d060d67e35be9967e670
tamir <tamirkhy@gmail.com>
2025-05-11 17:50:58 +0300

    Create email and password validator utils

---
+++ src/utils/auth/validators.ts
@@ -0,0 +1,8 @@
+export const isValidEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export const isStrongPassword = (password: string): boolean => {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+};
